5f37093be9eb4c5dec00dfa90b2a679df41eb9ba
spaartyyy <spaartyyy@gmail.com>
2015-11-10 20:28:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
       botRegexAd=/^\/advance/; botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /^\odb/i; botDE = /^\/derek/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/;
-      botHamad = /^\/Hamadsdad/;
+      botHamad = /^\/Hamadsdad/; botRing = /^\/ring/;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -63,6 +63,11 @@ function respond() {
   else if(request.text && botRegexSb.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://www.reddit.com/r/maddenall32");
+    this.res.end();
+  } 
+  else if(request.text && botRing.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://i.imgur.com/EKHM1GFh.jpg");
     this.res.end();
   } 
   else if(request.text && botRegexSh.test(request.text)) {
