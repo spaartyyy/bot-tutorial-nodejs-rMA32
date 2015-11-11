5564f345a2ee6b76158f63128f6c37407b4fd33a
spaartyyy <spaartyyy@gmail.com>
2015-11-10 18:31:33 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
       botRegexAd=/^\/advance/; botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /^\odb/i; botDE = /^\/derek/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/;
-      botHamad = /^\/Hamadsdad/; botCookie = /^\/cookie/; botCookiedance = /^\/cookie/dance/;
+      botHamad = /^\/Hamadsdad/;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -88,16 +88,6 @@ function respond() {
   else if(request.text && botDE.test(request.text)) {
     this.res.writeHead(200);
     postMessage("hes gay");
-    this.res.end();
-  } 
-  else if(request.text && botCookiedance.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("https://media.giphy.com/media/WP1PJnXWQN692/giphy.gif");
-    this.res.end();
-  } 
-  else if(request.text && botCookie.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("https://38.media.tumblr.com/f41ef9d622827cb3efbd8346cc989519/tumblr_mpcfgzVurk1qmkdzvo1_400.gif");
     this.res.end();
   } 
   else if(request.text && botHamad.test(request.text)) {
